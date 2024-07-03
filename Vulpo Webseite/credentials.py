75e1a91381a2ac8dfdcbc58a0cd1997af215c7f8
Vincent Lorenz <vinlo1357@gmail.com>
2024-07-03 21:38:19 +0200

    webseiten code gelöscht, ist seperat

--- Vulpo Webseite/credentials.py
+++
@@ -1,8 +0,0 @@
-client_id = 925799559576322078
-client_secret = 'MBwYPICsfr3umSi0OO0E6mrk-8LZDot0'
-base_discord_api_url = 'https://discord.com/api/v9'
-authorize_url = 'https://discordapp.com/api/v9/oauth2/authorize'
-token_url = 'https://discordapp.com/api/v9/oauth2/token'
-redirect_uri = 'https://vulpo-bot.de/oauth_callback'
-scope = ['identify','guilds']
-token="OTI1Nzk5NTU5NTc2MzIyMDc4.GcwvXN.EkMMDxTqykR8em6L4lJqOouGfvAvH1J1rq9nJQ"
