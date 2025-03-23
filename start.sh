fecafff82c274932f537b88379768faf193b3e71
Vinc135 <vinlo1357@gmail.com>
2025-03-23 09:29:05 +0100

    removed team member

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Start eingeleitet. Vulpo wird gestartet."
-screen -dmS Vulpo bash -c "python3 main.py; screen -XS $STY quit"
+screen -dmS Vulpo bash -c "source myenv/bin/activate; python3 main.py; screen -XS $STY quit"
 while true; do
 	if ! screen -ls | grep -q "Vulpo"; then
 		echo "Vulpo ist gecrasht. Ich starte Vulpo neu."
