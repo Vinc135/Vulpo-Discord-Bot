0ff421c0ccf5f04876981728470de353ff3070b8
Vincent <vulpobot@gmail.com>
2025-11-05 17:36:56 +0100

    bug fix

--- cogs/stats.py
+++ cogs/stats.py
@@ -469,7 +469,7 @@ class Stats(commands.Cog):
                     
                 await db["voicedata"].delete_one({"userID": str(member.id)})
                 if(time_in_minutes <= 1):
-                    return
+                    return 
                 
                 result = await db["voice"].find_one({"userID": str(member.id), "guildID": str(member.guild.id), "zeit": str(discord.utils.utcnow().__format__('%d.%m.%Y')), "channelID": str(before.channel.id)})
                 
